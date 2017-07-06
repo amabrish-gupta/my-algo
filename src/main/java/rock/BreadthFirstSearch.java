c214ba1040c56281052b9301776aa27764e622f1
ambarish <ambarishgupta89@gmail.com>
2017-07-06 22:56:48 +0530

    Update BreadthFirstSearch.java

--- src/main/java/rock/BreadthFirstSearch.java
+++ src/main/java/rock/BreadthFirstSearch.java
@@ -74,6 +74,8 @@ public class BreadthFirstSearch {
 }
 
 /**
+ Problem: 
+
  Consider an undirected graph consisting of  nodes where each node is labeled from  to  and the edge between any two nodes is always of length . We define node  to be the starting position for a BFS.
 
  Given  queries in the form of a graph and some starting node, , perform each query by calculating the shortest distance from starting node  to all the other nodes in the graph. Then print a single line of  space-separated integers listing node 's shortest distance to each of the  other nodes (ordered sequentially by node number); if  is disconnected from a node, print  as the distance to that node.
@@ -101,20 +103,12 @@ public class BreadthFirstSearch {
  3 1
  2 3
  2
+ 
  Sample Output
 
  6 6 -1
  -1 6
- Explanation
-
- We perform the following two queries:
-
- The given graph can be represented as:
- graph1
- where our start node, , is node . The shortest distances from  to the other nodes are one edge to node , one edge to node , and an infinite distance to node  (which it's not connected to). We then print node 's distance to nodes , , and  (respectively) as a single line of space-separated integers: 6, 6, -1.
- The given graph can be represented as:
- graph2
- where our start node, , is node . There is only one edge here, so node  is unreachable from node  and node  has one edge connecting it to node . We then print node 's distance to nodes  and  (respectively) as a single line of space-separated integers: -1 6.
+ 
  Note: Recall that the actual length of each edge is , and we print  as the distance to any node that's unreachable from .
  *
  *
